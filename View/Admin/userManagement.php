d9eb84a793c43320d83d40a310ff3cbac34d8947
bluecrypt47 <lechuong09291999@gmail.com>
2022-10-15 02:50:45 +0700

    update pagination user list

--- View/Admin/userManagement.php
+++ View/Admin/userManagement.php
@@ -59,9 +59,29 @@
                     $user1 = mysqli_fetch_assoc($result1);
                     // ===============================
 
-                    $sql = "SELECT * FROM users";
-                    $result = mysqli_query($conn, $sql);
-                    $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                    // Get all user in DB and pagination
+                    $count = 0;
+
+                    $countUsers = mysqli_query($conn, "SELECT count(*) as total FROM users");
+                    $rows = mysqli_fetch_assoc($countUsers);
+
+                    $totalRows = $rows['total'];
+
+                    $currentPage = isset($_GET['currentPage']) ? $_GET['currentPage'] : 1;
+                    $limit = 10;
+
+                    $sizePage = 2;
+                    $totalPage = ceil($totalRows / $limit);;
+
+                    if ($currentPage > $totalPage) {
+                        $currentPage = $totalPage;
+                    } else if ($currentPage < 1) {
+                        $currentPage = 1;
+                    }
+                    $start = ($currentPage - 1) * $limit;
+
+                    $result = mysqli_query($conn, "SELECT * FROM users LIMIT $start, $limit");
+
                     ?>
 
                     <!-- Content Row -->
@@ -83,81 +103,106 @@
                             </thead>
                             <tbody style="text-align: center;">
                                 <?php $i = 1;
-                                foreach ($users as $user) :
-                                    $phoneNumber = $user['phoneNumber'];
-                                    if ($phoneNumber == null) {
-                                        $phoneNumber = 'N/A';
-                                    }
-                                ?>
+                                while ($row = mysqli_fetch_assoc($result)) { ?>
                                     <tr>
                                         <th scope="row"><?php echo $i++; ?></th>
-                                        <td><?php echo $user['name']; ?></td>
-                                        <td><?php echo $user['email']; ?></td>
-                                        <td><?php echo $phoneNumber ?></td>
-                                        <td><?php if ($user['roles'] == 0) {
+                                        <td><?php echo $row['name']; ?></td>
+                                        <td><?php echo $row['email']; ?></td>
+                                        <td><?php echo $row['phoneNumber']; ?></td>
+                                        <td><?php if ($row['roles'] == 0) {
                                                 echo 'Admin';
                                             } else {
                                                 echo 'User';
                                             } ?></td>
                                         <td><?php echo $user['updateDate']; ?></td>
-                                        <td><a class="btn btn-info" title="View" href="viewDetail.php?id=<?php echo $user['id']; ?>"><i class="fas fa-eye"></i></a>
+                                        <td><a class="btn btn-info" title="View" href="viewDetail.php?id=<?php echo $row['id']; ?>"><i class="fas fa-eye"></i></a>
                                             <?php if ($user1['roles'] == 0) { ?>
-                                                <a class="btn btn-warning" title="Edit" href="updateUser.php?id=<?php echo $user['id']; ?>"><i class="fas fa-edit"></i></a>
-                                                <a class="btn btn-danger" title="Delete" href="delete.php?id=<?php echo $user['id']; ?>"><i class="fas fa-trash-alt"></i></a>
+                                                <a class="btn btn-warning" title="Edit" href="updateUser.php?id=<?php echo $row['id']; ?>"><i class="fas fa-edit"></i></a>
+                                                <a class="btn btn-danger" title="Delete" href="delete.php?id=<?php echo $row['id']; ?>"><i class="fas fa-trash-alt"></i></a>
                                             <?php } ?>
-
                                         </td>
                                     </tr>
-                                <?php endforeach; ?>
+                                <?php }
+                                ?>
                             </tbody>
                         </table>
 
                         <div>
                             <a class="btn btn-success" href="<?php echo $_SERVER['HTTP_REFERER']; ?>"><i class="fas fa-caret-left"></i> Back</a>
                         </div>
+                        <hr>
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination">
+                                <li class="page-item">
+                                    <?php if ($currentPage > 1 && $totalPage > 1) { ?>
+                                        <a class="page-link" href="userManagement.php?currentPage=' . ($currentPage - 1) . '" aria-label="Previous">
+                                            <span aria-hidden="true">&laquo;</span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <?php
+                                    }
+
+                                    for ($i = 1; $i <= $totalPage; $i++) {
+                                        if ($i == $currentPage) { ?>
+                                <li class="page-item"><a class="page-link" href="#"><?php echo $i; ?></a></li>
+                            <?php  } else { ?>
+
+                                <li class="page-item"><a class="page-link" href="userManagement.php?currentPage=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                            <?php   }
+                                    }
+                                    if ($currentPage < $totalPage && $totalPage > 1) { ?>
+                            <li class="page-item">
+                                <a class="page-link" href="userManagement.php?currentPage=<?php echo $currentPage + 1; ?>" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </li>
+                        <?php  }
+                        ?>
+                        </li>
+                            </ul>
+                        </nav>
 
                     </div>
+                    <!-- /.container-fluid -->
 
                 </div>
-                <!-- /.container-fluid -->
+                <!-- End of Main Content -->
+
+                <!-- Footer -->
+                <?php require 'D:\DVWA\ProjectCTF\View\Layout\footer.php'; ?>
+                <!-- End of Footer -->
 
             </div>
-            <!-- End of Main Content -->
-
-            <!-- Footer -->
-            <?php require 'D:\DVWA\ProjectCTF\View\Layout\footer.php'; ?>
-            <!-- End of Footer -->
+            <!-- End of Content Wrapper -->
 
         </div>
-        <!-- End of Content Wrapper -->
+        <!-- End of Page Wrapper -->
 
-    </div>
-    <!-- End of Page Wrapper -->
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
+        <!-- Logout Modal-->
+        <?php require 'D:\DVWA\ProjectCTF\View\Layout\logoutModal.php' ?>
 
-    <!-- Logout Modal-->
-    <?php require 'D:\DVWA\ProjectCTF\View\Layout\logoutModal.php' ?>
+        <!-- Bootstrap core JavaScript-->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Core plugin JavaScript-->
+        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+        <!-- Custom scripts for all pages-->
+        <script src="js/sb-admin-2.min.js"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+        <!-- Page level plugins -->
+        <script src="vendor/chart.js/Chart.min.js"></script>
 
-    <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
+        <!-- Page level custom scripts -->
+        <script src="js/demo/chart-area-demo.js"></script>
+        <script src="js/demo/chart-pie-demo.js"></script>
 
 </body>
 
